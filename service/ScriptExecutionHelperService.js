8b1929b595279151bee10647fd263f5775a6b35f
Christian Danowski-Buhren <christian.danowski-buhren@hs-bochum.de>
2018-08-16 16:01:35 +0200

    add draft for a helper module to fetch required resources and execute process script

---
+++ service/ScriptExecutionHelperService.js
@@ -0,0 +1,62 @@
+'use strict';
+
+var KomMonitorDataFetcher = require('./KomMonitorDataFetcherService');
+
+// aquire connection details to KomMonitor data management api instance from environment variables
+const kommonitorDataManagementHost = process.env.KOMMONITOR_DATA_MANAGEMENT_HOST;
+const kommonitorDataManagementPort = process.env.KOMMONITOR_DATA_MANAGEMENT_PORT;
+const kommonitorDataManagementBasepath = process.env.KOMMONITOR_DATA_MANAGEMENT_BASEPATH;
+
+// construct fixed starting URL to make requests against running KomMonitor data management api
+const kommonitorDataManagementURL = kommonitorDataManagementHost + ":" + kommonitorDataManagementPort + kommonitorDataManagementBasepath;
+
+async function executeDefaultComputation(job, scriptId, targetDate, baseIndicatorIds, georesourceIds){
+  // TODO for each spatial unit perform script execution, receive response GeoJSON and make POST call to data management API
+  // TODO for that compute for the lowest spatial unit and after that aggregate to all superior units!
+  // TODO also receive default parameter values for script execution from data management API, should not be part of the script itself
+  return new Promise(function(resolve, reject) {
+
+    try {
+      // var scriptCodeAsString = await KomMonitorDataFetcher.fetchScriptCodeById(kommonitorDataManagementURL, scriptId);
+      // var baseIndicatorsMap = await KomMonitorDataFetcher.fetchIndicatorsByIds(kommonitorDataManagementURL, baseIndicatorIds, targetDate, "Stadtteilebene");
+      // var georesourcesMap = null;
+      //
+      // var lowestSpatialUnit_geoJSON = "";
+      // //map of objects
+      // var superiorSpatialUnits = "";
+      // var defaultProcesParameters = "";
+
+      // TODO best as map/array of URLs since for each spatial unit a different indicator dataset is computed.
+      var urlToCreatedResource = "";
+      resolve(urlToCreatedResource);
+    }
+    catch(err) {
+        consol.log("Error during execution of defaultIndicatorComputation with error: " + err);
+        resolve(err);
+    }
+  });
+}
+
+async function executeCustomizedComputation(job, scriptId, targetDate, baseIndicatorIds, georesourceIds, targetSpatialUnitId, customProcessProperties){
+  // TODO for the target spatial unit perform script execution, receive response GeoJSON and return it
+  return new Promise(function(resolve, reject) {
+
+    try {
+      // var scriptCodeAsString = await KomMonitorDataFetcher.fetchScriptCodeById(kommonitorDataManagementURL, scriptId);
+      // var baseIndicatorsMap = await KomMonitorDataFetcher.fetchIndicatorsByIds(kommonitorDataManagementURL, baseIndicatorIds, targetDate, "Stadtteilebene");
+      // var georesourcesMap = null;
+      //
+      // var lowestSpatialUnit_geoJSON = "";
+      // //map of objects
+      // var superiorSpatialUnits = "";
+      // var defaultProcesParameters = "";
+
+      var responseGeoJson = "";
+      resolve(responseGeoJson);
+    }
+    catch(err) {
+        consol.log("Error during execution of customizedIndicatorComputation with error: " + err);
+        resolve(err);
+    }
+  });
+}
