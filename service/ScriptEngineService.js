198830dba5cf18f5372d0580f204beda3e9d17f2
Christian Danowski-Buhren <christian.danowski-buhren@hs-bochum.de>
2018-08-16 09:53:35 +0200

    remove old outcommented sections and cleanup formating

--- service/ScriptEngineService.js
+++ service/ScriptEngineService.js
@@ -39,16 +39,6 @@
     console.log("Saving job, which was enriched with result: " + job.data.result);
     job.data.progress = 100;
     job.save();
-    // .then((job) => {
-    //   console.log("Inserted result to job with id " + job.id);
-    //
-    //   console.log("Result is " + job.data.result);
-    //   resolve();
-    // });
-
-    // var responsePayload = new ResponsePayload(201, simpleTestResult);
-    //
-    // console.log("processing finished. ResultObject is: " + responsePayload);
 
     console.log(`Job execution successful. Job with ID ${job.id} finished`);
     return done();
@@ -123,30 +113,8 @@ exports.getDefaultIndicatorComputation = function(jobId) {
       console.log(response);
 
       resolve(response);
+    });
   });
-});
-
-//   return new Promise(function(resolve, reject) {
-//     var examples = {};
-//     examples['application/json'] = [ {
-//   "jobId" : "jobId",
-//   "result_url" : "result_url",
-//   "progress" : 0.80082819046101150206595775671303272247314453125,
-//   "error" : "error",
-//   "status" : "ACCEPTED"
-// }, {
-//   "jobId" : "jobId",
-//   "result_url" : "result_url",
-//   "progress" : 0.80082819046101150206595775671303272247314453125,
-//   "error" : "error",
-//   "status" : "ACCEPTED"
-// } ];
-//     if (Object.keys(examples).length > 0) {
-//       resolve(examples[Object.keys(examples)[0]]);
-//     } else {
-//       resolve();
-//     }
-//   });
 }
 
 
@@ -178,10 +146,6 @@ exports.postDefaultIndicatorComputation = function(scriptInput) {
   // use bee-queue to create a new queue and new job to execute the script
   return new Promise(function(resolve, reject) {
     const job = defaultComputationQueue.createJob(scriptInput);
-
-    //job.timeout(10000).retries(2).save().then((job) => {
-      // job enqueued, job.id populated
-    //});
 
     job.save().then((job) => {
       console.log("Created new job with jobId " + job.id);
